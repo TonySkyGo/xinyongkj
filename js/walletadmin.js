cc96f3b94451276925edba490e7ea714db60a395
chenchun <913690912@qq.com>
2017-07-21 14:13:36 +0800

    改支”钱包管理“ 页面各种细节。

--- js/walletadmin.js
+++ js/walletadmin.js
@@ -130,9 +130,12 @@ $(document).ready(function() {
         }
     }
     //“运费模版”添加“运送到”空行
-    //添加一行
+
+    //添加“为指定地区城市设置运费”包裹记重行
     var htmlval='<tr id="modeltr"><td class="model-td1"><input type="checkbox" class="areacheck" name="area"><div class="model-area">未添加地区</div><div class="model-edit">编辑</div></td><td class="model-td2"><input type="text" class="model-input"></td><td class="model-td2"><input type="text" class="model-input"></td><td class="model-td2"><input type="text" class="model-input"></td><td class="model-td2"><input type="text" class="model-input"></td><td class="model-td3"><span class="model-delete" id="model-delete" onclick="modeldelete(this)">删除</span></td></tr>';
+    //添加“为指定地区城市设置运费”选择运送方式行
     var htmlvals='<tr><td class="model-td1"><input type="checkbox" class="areacheck" name="area"><div class="model-area">未添加地区</div><div class="model-edit">编辑</div></td><td class="model-td2 model-vet"><select class="model-select" id="model-select"><option value="1">申通</option><option value="2">韵达</option><option value="3">中通</option></select></td><td class="model-td2"><p class="model-express" id="getselectval">申通快递</p></td><td class="model-td4 model-vet" colspan="2"  id="selectdataval"><select class="model-select" id="model-selectdata"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select><div class="model-full"><span class="model-full-word">满</span><input type="text" class="model-input model-input-unit" id="selectdataval"><span class="model-unit">件</span>包邮</div></td><td class="model-td3"><span class="model-delete"  onclick="modeldeletes(this)">删除</span></td></tr>';
+    //添加一行
     $("#model-add").click(function () {
         $("#modelnull").find("tr").eq(0).after(htmlval);
     });
@@ -155,13 +158,12 @@ $(document).ready(function() {
         });
     }
     //.......
-
-   // $("select#model-select").change(function(){
+   // 选择快递
     $(document).on("change", "select#model-select", function () {
         var ctext=$(this).find("option:selected").text();
         $(this).parent().parent().find("td p#getselectval").html(ctext+"快递");
     })
-    //$("select#model-selectdata").change(function(){
+    //选择“件”数
     $(document).on("change", "select#model-selectdata", function () {
         var ctext=$(this).find("option:selected").text();
         $(this).parent().find("div input#selectdataval").val(ctext);
